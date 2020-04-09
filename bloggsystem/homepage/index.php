a7a3d0ca8e17727c2e2ac7da4ba3490805880e9d
Khoi <khoii87@gmail.com>
2020-04-09 03:02:25 +0200

    add: :pencil: Add comments on the code

--- bloggsystem/homepage/index.php
+++ bloggsystem/homepage/index.php
@@ -16,10 +16,13 @@
             <form action="" method="post">
             <select name="kategori" id="kategori">';
                 <?php 
+                    // Vi tar spørring mot databasen av tabellen category
                     $sql = "SELECT * FROM category";
-                    $resultat = mysqli_query($db_connect, $sql);                                    
+                    // Vi henter spørringer fra databasen
+                    $resultat = mysqli_query($db_connect, $sql);     
+                    // Returnerer assosiativ tabell            
                     while($row = mysqli_fetch_assoc($resultat)) {
-                        // Generer Nedtrekk Liste
+                        // Vi henter kolonnen fra spørringen av tabellen
                         $cat_name = $row['name'];
                         echo "<option value=$cat_name>" . $cat_name ."</option>";
                     }
@@ -30,24 +33,22 @@
         </div>
         <div class="col-xl-6">
             <?php 
+                // isset() sjekker om det er true/false. Her sjekker vi om "submit" knappen eksisterer
+                // isåfall, så returnerer den true
                 if(isset($_POST['submit'])) {
+                    // Vi tar enda en ny spørring etter vi har submitet 
                    $sql = "SELECT * 
                            FROM category 
                            INNER JOIN postcategory ON category.id = postcategory.id 
                            INNER JOIN blogpost ON  postcategory.blogpostid = blogpost.id";
 
                     $resultat = mysqli_query($db_connect, $sql);
-
                     while($row = mysqli_fetch_assoc($resultat)) {
                         $blog_name = $row['name'];
                         $blog_title = $row['title'];
-                        switch ($_POST['kategori'] == $blog_name) {
-                        case $blog_name:
+                        
+                        if ($_POST['kategori'] == $blog_name) {
                            echo '<p>' . $blog_title . '</p>';
-                           break;
-                        default:
-                           # code...
-                           break;
                         }
                     } 
                 } 
